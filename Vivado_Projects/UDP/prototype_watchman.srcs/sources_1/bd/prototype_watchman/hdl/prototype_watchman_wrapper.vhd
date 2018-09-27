dc5a32f60ca692574a58debb3e002c26fabf2a05
IDLAB-52 <kyho35@hawaii.edu>
2018-09-27 11:05:02 -1000

    Moved reg map to PL, 15 reg in regmap; 10 write, 5 read

--- Vivado_Projects/UDP/prototype_watchman.srcs/sources_1/bd/prototype_watchman/hdl/prototype_watchman_wrapper.vhd
+++ Vivado_Projects/UDP/prototype_watchman.srcs/sources_1/bd/prototype_watchman/hdl/prototype_watchman_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.1 (lin64) Build 2188600 Wed Apr  4 18:39:19 MDT 2018
---Date        : Mon Aug 13 11:41:04 2018
+--Date        : Tue Sep 25 13:12:53 2018
 --Host        : idlab52-OptiPlex-790 running 64-bit Ubuntu 14.04.5 LTS
 --Command     : generate_target prototype_watchman_wrapper.bd
 --Design      : prototype_watchman_wrapper
